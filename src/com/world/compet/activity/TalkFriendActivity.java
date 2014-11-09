b669ba2d551f7b8c20aedd738ef5f1612465f364
huanggui <353340273@qq.com>
2014-11-09 23:26:57 +0800

    添加2个入口activity。

---
+++ src/com/world/compet/activity/TalkFriendActivity.java
@@ -0,0 +1,15 @@
+package com.world.compet.activity;
+
+import com.world.compet.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TalkFriendActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_talkfriends_layout);
+	}
+}
