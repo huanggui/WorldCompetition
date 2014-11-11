700c0c3fab7fc2d453c29cf26de65e4fd2b4e66e
Administrator <Administrator@ighuang-PC1.tencent.com>
2014-11-11 21:46:55 +0800

    竞赛页顶部是3个下拉菜单，而不是3个viewpager。

--- src/com/world/compet/activity/CompetitionTabActivity.java
+++ src/com/world/compet/activity/CompetitionTabActivity.java
@@ -1,65 +1,43 @@
 package com.world.compet.activity;
 
 import com.world.compet.R;
-import com.world.compet.adpter.TXTabViewPageAdapter;
-import com.world.compet.component.TXTabViewPage;
-import com.world.compet.component.TXTabViewPage.ITXTabViewPageListener;
-import com.world.compet.view.CompetitionLevelView;
-import com.world.compet.view.CompetitionTimeView;
-import com.world.compet.view.CompetitionTypeView;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.FrameLayout;
+public class CompetitionTabActivity extends Activity{
 
-public class CompetitionTabActivity extends Activity implements ITXTabViewPageListener{
-
-	private CompetitionTypeView mCompetitionTypeView;
-	private CompetitionLevelView mCompetitionLevelView;
-	private CompetitionTimeView mCompetitionTimeView;
-	private TXTabViewPage mViewPage;
-	private int mPageIndex = 0;
+//	private CompetitionTypeView mCompetitionTypeView;
+//	private CompetitionLevelView mCompetitionLevelView;
+//	private CompetitionTimeView mCompetitionTimeView;
+//	private TXTabViewPage mViewPage;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_competition_layout);
-		createPageViews();
 	}
 
-	private void createPageViews() {
-		FrameLayout contentView = (FrameLayout)this.findViewById(R.id.content_view);
-		mViewPage = new TXTabViewPage(this);
-		
-		TXTabViewPageAdapter adapter = new TXTabViewPageAdapter();
-		
-		mCompetitionTypeView = new CompetitionTypeView(this);
-		mCompetitionLevelView = new CompetitionLevelView(this);
-		mCompetitionTimeView = new CompetitionTimeView(this);
-
-		String title1 = this.getString(R.string.competition_type_title);
-		String title2 = this.getString(R.string.competition_level_title);
-		String title3 = this.getString(R.string.competition_time_title);
-		adapter.addPageItem(title1, mCompetitionTypeView);
-		adapter.addPageItem(title2, mCompetitionLevelView);
-		adapter.addPageItem(title3, mCompetitionTimeView);
-		mViewPage.setAdapter(adapter);
-		//mViewPage.setListener(this);			
-		contentView.addView(mViewPage, LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-		mViewPage.setPageSelected(mPageIndex);
-	}
-
-	public void onTxTabViewPageSelected(int position) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onTxTabViewPageWillSelect(int position) {
-		// TODO Auto-generated method stub
-		
-	}
+//	private void createPageViews() {
+//		FrameLayout contentView = (FrameLayout)this.findViewById(R.id.content_view);
+//		mViewPage = new TXTabViewPage(this);
+//		
+//		TXTabViewPageAdapter adapter = new TXTabViewPageAdapter();
+//		
+//		mCompetitionTypeView = new CompetitionTypeView(this);
+//		mCompetitionLevelView = new CompetitionLevelView(this);
+//		mCompetitionTimeView = new CompetitionTimeView(this);
+//
+//		String title1 = this.getString(R.string.competition_type_title);
+//		String title2 = this.getString(R.string.competition_level_title);
+//		String title3 = this.getString(R.string.competition_time_title);
+//		adapter.addPageItem(title1, mCompetitionTypeView);
+//		adapter.addPageItem(title2, mCompetitionLevelView);
+//		adapter.addPageItem(title3, mCompetitionTimeView);
+//		mViewPage.setAdapter(adapter);
+//		//mViewPage.setListener(this);			
+//		contentView.addView(mViewPage, LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+//		mViewPage.setPageSelected(mPageIndex);
+//	}
 
 	
 }
--- src/com/world/compet/activity/UserTabActivity.java
+++ src/com/world/compet/activity/UserTabActivity.java
@@ -1,16 +1,9 @@
 package com.world.compet.activity;
 
 import com.world.compet.R;
-import com.world.compet.adpter.TXTabViewPageAdapter;
-import com.world.compet.component.TXTabViewPage;
-import com.world.compet.view.CompetitionLevelView;
-import com.world.compet.view.CompetitionTimeView;
-import com.world.compet.view.CompetitionTypeView;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.FrameLayout;
 
 public class UserTabActivity extends Activity {
 	@Override
